b25242d9988d9788075dd5317e1ba4501ae14134
Jhonattan Millán <jhonattan.millan504@gmail.com>
2024-10-14 05:27:07 -0400

    Add swagger document configuration

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const APP = await NestFactory.create(AppModule);
   APP.setGlobalPrefix('api');
+
+  const SWAGGER_CONFIG = new DocumentBuilder()
+    .setTitle('Amazn API')
+    .setDescription('API for Amazn eShop.')
+    .setVersion('1.0')
+    .build();
+  const DOCUMENT = SwaggerModule.createDocument(APP, SWAGGER_CONFIG);
+  SwaggerModule.setup('api/docs', APP, DOCUMENT);
+
   await APP.listen(3000);
 }
 bootstrap();
