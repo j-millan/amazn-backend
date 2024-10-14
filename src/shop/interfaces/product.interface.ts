30c4a711875668c7cc72e28ecb2b052ed13f74d6
Jhonattan Millán <jhonattan.millan504@gmail.com>
2024-10-14 05:19:30 -0400

    Create interface for product and add dummy data

---
+++ src/shop/interfaces/product.interface.ts
@@ -0,0 +1,11 @@
+export interface ProductInterface {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  images: [];
+  reviews: [];
+  createdAt: string;
+  updatedAt: string;
+}
--- src/shop/services/products.service.ts
+++ src/shop/services/products.service.ts
@@ -1,23 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { ProductInterface } from '../interfaces';
+import { PRODUCTS_DATA } from '../data/products';
 
 @Injectable()
 export class ProductsService {
-  private readonly _PRODUCTS: any[] = [
-    { id: 1, description: 'Product 1' },
-    { id: 2, description: 'Product 2' },
-    { id: 3, description: 'Product 3' },
-    { id: 4, description: 'Product 4' },
-  ];
+  private readonly _PRODUCTS: ProductInterface[] = PRODUCTS_DATA;
 
-  async getAll(): Promise<any[]> {
+  async getAll(): Promise<ProductInterface[]> {
     return this._PRODUCTS;
   }
 
-  async find(id: number): Promise<any> {
+  async find(id: number): Promise<ProductInterface> {
     return this._PRODUCTS.find((product) => product.id === id);
   }
 
-  async create(data: any): Promise<void> {
+  async create(data: ProductInterface): Promise<void> {
     this._PRODUCTS.push(data);
   }
 
