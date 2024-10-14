124780a118e45f7d540d21a7f571fc2c76ed51e4
Jhonattan Millán <jhonattan.millan504@gmail.com>
2024-10-14 05:24:10 -0400

    Set global prefix for api

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const APP = await NestFactory.create(AppModule);
+  APP.setGlobalPrefix('api');
+  await APP.listen(3000);
 }
 bootstrap();
