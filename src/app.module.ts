ea0cc0324b6740edbc848f5300bb661636145ab5
Jhonattan Millán <jhonattan.millan504@gmail.com>
2024-10-23 18:02:52 -0400

    Setup config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ShopModule } from './shop/shop.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+const configModule = ConfigModule.forRoot({
+  envFilePath: '.env.local',
+});
 
 @Module({
-  imports: [ShopModule],
+  imports: [configModule, typeOrmModule, ShopModule],
   controllers: [],
   providers: [],
 })
