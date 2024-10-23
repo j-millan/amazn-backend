55923dec4c5c8b1e2fcfcf58b2d01652c28400e5
Jhonattan Millán <jhonattan.millan504@gmail.com>
2024-10-23 18:03:11 -0400

    Set up typeorm module for db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ShopModule } from './shop/shop.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+
+const typeOrmModule = TypeOrmModule.forRootAsync({
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: (configService) => ({
+    type: 'postgres',
+    host: configService.get('DB_HOST'),
+    port: configService.get('DB_PORT'),
+    username: configService.get('DB_USERNAME'),
+    password: configService.get('DB_PASSWORD'),
+    database: configService.get('DB_NAME'),
+    entities: [],
+    synchronize: true,
+  }),
+});
 
 const configModule = ConfigModule.forRoot({
   envFilePath: '.env.local',
