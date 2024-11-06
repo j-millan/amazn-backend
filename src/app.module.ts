606d129b5f62f8bd1cc1b1db5f16f9331364dcab
Jhonattan Millán <jhonattan.millan504@gmail.com>
2024-11-05 23:11:26 -0400

    Crete auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { ShopModule } from './shop/shop.module';
 import { ConfigModule } from '@nestjs/config';
 import { typeOrmConfig } from './config/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 const configModule = ConfigModule.forRoot({
   envFilePath: '.env.local',
 });
 
 @Module({
-  imports: [configModule, typeOrmConfig, ShopModule],
+  imports: [configModule, typeOrmConfig, ShopModule, AuthModule],
   controllers: [],
   providers: [],
 })
