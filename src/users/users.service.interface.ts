f6658faa2c828ea9d3ea66f78840a4043506c872
Jhonattan Millán <jhonattan.millan504@gmail.com>
2024-11-06 23:02:15 -0400

    Create service for user creation

--- src/users/users.service.interface.ts
+++ src/users/users.service.interface.ts
@@ -1,5 +1,7 @@
 import { User } from 'src/users/entities';
+import { CreateUserDto } from './dto';
 
 export interface UserServiceInterface {
   find(id: string): Promise<User>;
+  create(data: CreateUserDto): Promise<User>;
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable, HttpStatus } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 import { UserServiceInterface } from './users.service.interface';
 import { User } from './entities';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto';
 
 @Injectable()
 export class UsersService implements UserServiceInterface {
@@ -10,5 +11,20 @@ export class UsersService implements UserServiceInterface {
 
   async find(id: string): Promise<User> {
     return await this._usersRepo.findOne({ where: { id } });
+  }
+
+  async create(data: CreateUserDto): Promise<User> {
+    if (!data.email && !data.phoneNumber) {
+      throw new HttpException(
+        'Email or phone number is required',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const NEW_USER = await this._usersRepo.save(
+      await this._usersRepo.create(data),
+    );
+
+    return NEW_USER;
   }
 }
