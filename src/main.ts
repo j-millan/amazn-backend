d4fc851eff4d741fd59f80a242f984afdbefde53
Jhonattan Millán <jhonattan.millan504@gmail.com>
2024-11-06 23:38:17 -0400

    Add validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const APP = await NestFactory.create(AppModule);
   APP.setGlobalPrefix('api');
+  APP.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   const SWAGGER_CONFIG = new DocumentBuilder()
     .setTitle('Amazn API')
