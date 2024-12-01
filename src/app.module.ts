f8cc2efa73406b2e3a67e37c6860a5d6db14554b
Jhonattan Millán <jhonattan.millan504@gmail.com>
2024-11-30 20:03:28 -0400

    Install nestjs schedule package

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TYPE_ORM_CONFIG } from './core';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 const CONFIG_MODULE = ConfigModule.forRoot({
   envFilePath: '.env.local',
@@ -14,6 +15,7 @@ const CONFIG_MODULE = ConfigModule.forRoot({
   imports: [
     CONFIG_MODULE,
     TYPE_ORM_CONFIG,
+    ScheduleModule.forRoot(),
     ShopModule,
     AuthModule,
     UsersModule,
