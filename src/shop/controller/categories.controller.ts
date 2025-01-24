7b850335850dcd69e39d1647343c98f37f0ec0c6
Jhonattan Millán <jhonattan.millan504@gmail.com>
2025-01-23 21:54:19 -0400

    Create categories controller

---
+++ src/shop/controller/categories.controller.ts
@@ -0,0 +1,67 @@
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  Inject,
+  Param,
+  Res,
+} from '@nestjs/common';
+import {
+  ApiNoContentResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+import { Response } from 'express';
+
+import { Category } from '../entities';
+import { ShopInjectionEnum } from '../enums';
+import { CategoriesService } from '../services';
+
+@Controller('categories')
+@ApiTags('categories')
+export class CategoriesController {
+  constructor(
+    @Inject(ShopInjectionEnum.CATEGORIES_SERVICE)
+    private _categoriesService: CategoriesService,
+  ) {}
+
+  @Get()
+  @ApiOperation({ summary: 'Fetch all categories.' })
+  @ApiOkResponse({
+    description: 'OK',
+    type: () => Category,
+    isArray: true,
+  })
+  @ApiNoContentResponse({ description: 'No Content' })
+  async getCategories(@Res() response: Response): Promise<Response> {
+    const RESULT = await this._categoriesService.getAll();
+
+    if (!RESULT.length) {
+      return response.status(HttpStatus.NO_CONTENT).send();
+    }
+
+    response.status(HttpStatus.OK).json(RESULT);
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: 'Find category by ID.' })
+  @ApiOkResponse({
+    description: 'OK',
+    type: () => Category,
+  })
+  @ApiNotFoundResponse({ description: 'No Content' })
+  async getCategory(
+    @Res() response: Response,
+    @Param('id') id: number,
+  ): Promise<Response> {
+    const RESULT = await this._categoriesService.find(null, id);
+
+    if (!RESULT) {
+      return response.status(HttpStatus.NOT_FOUND).send();
+    }
+
+    response.status(HttpStatus.OK).json(RESULT);
+  }
+}
--- src/shop/shop.module.ts
+++ src/shop/shop.module.ts
@@ -4,11 +4,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ShopInjectionEnum } from './enums';
 import { Category, Product } from './entities';
 import { CategoriesService, ProductsService } from './services';
-import { ProductsController } from './controller/products.controller';
+import { CategoriesController, ProductsController } from './controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Product, Category])],
-  controllers: [ProductsController],
+  controllers: [ProductsController, CategoriesController],
   providers: [
     {
       provide: ShopInjectionEnum.PRODUCTS_SERVICE,
